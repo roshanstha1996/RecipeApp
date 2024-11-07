f6044aedd8d08d5b15066e9fbeb3f496ff0256b0
roshan <roshanstha1996@gmail.com>
2024-11-07 12:44:02 -0500

    Initial Commit

---
+++ ROSHAN_Recipes/ROSHAN_RecipesApp.swift
@@ -0,0 +1,17 @@
+//
+//  ROSHAN_RecipesApp.swift
+//  ROSHAN_Recipes
+//
+//  Created by Aswin Dahal on 2024-11-07.
+//
+
+import SwiftUI
+
+@main
+struct ROSHAN_RecipesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
